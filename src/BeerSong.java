493b57a0127f5b6325787e5a95c2537e6b469ef2
Brandon Scoppa <scoppa.brandon@gmail.com>
2019-02-06 16:24:31 -0500

    Added program output and end program output println's

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -1,6 +1,8 @@
 public class BeerSong {
     public static void main(String[] args) {
+        System.out.println("*************** Program output ***************************");
         Ninety_Nine_Bottles_Of_Beer();
+        System.out.println("*************** End program output ***************************");
     }
 
     public static void Ninety_Nine_Bottles_Of_Beer() {
